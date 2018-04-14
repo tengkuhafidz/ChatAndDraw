4c46e5527306946c324738ce9bcc746893f3eb21
Tengku Hafidz <tengkuhafidz@gmail.com>
2018-04-14 14:27:04 +0800

    fix frontend initializing

--- chat-client/src/api.js
+++ chat-client/src/api.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('http://172.25.101.194:4000');
+const socket = io('http://localhost:4000');
 
 const sendMessage = (message) => {
 	socket.emit('chat', message);
